7a8f82e62d3f0223754030caf54cd412f126b024
Skablam <matthew.pease@landregistry.gsi.gov.uk>
2016-05-09 15:38:17 +0100

    changed ; to &&

--- scripts/vagrant-plugin-installer.rb
+++ scripts/vagrant-plugin-installer.rb
@@ -1,5 +1,5 @@
 def install_plugins(plugins)
   plugins.each do |plugin|
-    exec "vagrant plugin install #{plugin};vagrant #{ARGV.join(" ")}" unless Vagrant.has_plugin? plugin || ARGV[0] == 'plugin'
+  	exec "vagrant plugin install #{plugin} && vagrant #{ARGV.join(" ")}" unless (Vagrant.has_plugin? plugin || ARGV[0] == 'plugin')
   end
 end
