9991563921a8ad67eba4a062a87fa8ae3744bfb2
Simon Chapman <simon.chapman@digital.landregistry.gov.uk>
2016-05-11 14:50:24 +0000

    No longer needed

--- scripts/vagrant-plugin-installer.rb
+++
@@ -1,5 +0,0 @@
-def install_plugins(plugins)
-  plugins.each do |plugin|
-  	exec "vagrant plugin install #{plugin} && vagrant #{ARGV.join(" ")}" unless (Vagrant.has_plugin? plugin || ARGV[0] == 'plugin')
-  end
-end
