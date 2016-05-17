22e5c63a4bd9407f677c3377e7f70908187f82d5
Simon Chapman <sdchapman@gmail.com>
2016-05-17 23:59:50 +0100

    Rework dependencies to have dynamically loaded compose fragments

--- scripts/guest/docker/docker-provision.sh
+++ scripts/guest/docker/docker-provision.sh
@@ -1,5 +1,8 @@
+# Load all the -f docker compose file references that were saved earlier
+dockerfilelist=$(</vagrant/.docker-compose-file-list)
+
 echo "- - - (Re)building container images (volumes kept) - - -"
-/usr/local/bin/docker-compose -f /vagrant/scripts/guest/docker/docker-compose.yml build
+/usr/local/bin/docker-compose $dockerfilelist build
 
 echo "- - - Creating and launching containers - - -"
-/usr/local/bin/docker-compose -f /vagrant/scripts/guest/docker/docker-compose.yml up --no-build -d
+/usr/local/bin/docker-compose $dockerfilelist up --no-build -d
--- scripts/host/preparing_docker_compose.rb
+++ scripts/host/preparing_docker_compose.rb
@@ -5,31 +5,18 @@ def prepare_compose(root_loc)
   root_loc = root_loc
   # Load configuration.yml into a Hash
   config = YAML.load_file("#{root_loc}/dev-env-project/configuration.yml")
-
+  
+  # Put all the dependencies for all apps into an array, as their compose file argument
   dependency_list = []
   config["applications"].each do |appname, appconfig|
-    dependency_list.push(*appconfig["dependencies"])
-  end
-
-  docker_compose = YAML::load_file("#{root_loc}/scripts/guest/docker/docker-compose.template")
-
-  #Remove duplicate dependencies
-  dependency_list = dependency_list.uniq
-
-  dependency_list.each do |dependency|
-    if dependency == "postgres"
-      postgres = {}
-      docker_compose["services"]["postgres"] = postgres
-      postgres["container_name"] = "postgres"
-      postgres["build"] = "/vagrant/scripts/guest/docker/postgres"
-      postgres["ports"] = ["5432:5432"]
+    appconfig["dependencies"].each do |dependency|
+      dependency_list.push("-f /vagrant/scripts/guest/docker/#{dependency}/docker-compose-fragment.yml")
     end
   end
 
-  File.open("#{root_loc}/scripts/guest/docker/docker-compose.yml", 'w') {|f| f.write docker_compose.to_yaml } #Store
-end
+  # Remove duplicate dependencies
+  dependency_list = dependency_list.uniq
 
-#If this script is run directly
-if __FILE__ == $0
-  prepare_compose(File.dirname(__FILE__) + "../../")
+  # Put the compose arguments into a file for later retrieval
+  File.open("#{root_loc}/.docker-compose-file-list", 'w') {|f| f.write(dependency_list.join(" "))}
 end
