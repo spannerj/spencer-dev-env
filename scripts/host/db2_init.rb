5949b863cc856a6696e80d98b0a23e20e8178569
Skablam <stainlesssteelmatt@gmail.com>
2016-05-20 15:02:10 +0100

    renamed postgres to db2 in db2_init.rb

--- scripts/host/db2_init.rb
+++ scripts/host/db2_init.rb
@@ -8,14 +8,14 @@ def prepare_db2(root_loc)
   config = YAML.load_file("#{root_loc}/dev-env-project/configuration.yml")
 
   # Write the line we always need
-  File.open("#{root_loc}/scripts/guest/docker/postgres/init.sql", 'w') { |file| file.write("CREATE ROLE vagrant WITH LOGIN PASSWORD 'vagrant';") }
+  File.open("#{root_loc}/scripts/guest/docker/db2/init.sql", 'w') { |file| file.write("") }
 
   config["applications"].each do |appname, appconfig|
     # Load any SQL contained in the apps into the master file
-    if File.exists?("#{root_loc}/apps/#{appname}/postgres-init-fragment.sql")
+    if File.exists?("#{root_loc}/apps/#{appname}/db2-init-fragment.sql")
       puts colorize_pink("Found some in #{appname}")
-      to_append = File.read("#{root_loc}/apps/#{appname}/postgres-init-fragment.sql")
-      File.open("#{root_loc}/scripts/guest/docker/postgres/init.sql", 'a') { |file| file.puts to_append }
+      to_append = File.read("#{root_loc}/apps/#{appname}/db2-init-fragment.sql")
+      File.open("#{root_loc}/scripts/guest/docker/db2/init.sql", 'a') { |file| file.puts to_append }
     end
   end
 
