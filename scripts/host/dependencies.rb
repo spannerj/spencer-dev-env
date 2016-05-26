54d905a7c3cf51cf20afabccf465b233fbc5702b
SimonChapman <simon.chapman@landregistry.gov.uk>
2016-05-26 11:04:03 +0100

    Bug fix and allow new dependencies to be added dynamically

--- scripts/host/dependencies.rb
+++ scripts/host/dependencies.rb
@@ -1,38 +1,46 @@
 require_relative 'utilities'
+require 'yaml'
 
 def create_dependencies_list(root_loc)
+  root_loc = root_loc
+  # Load configuration.yml into a Hash
+  config = YAML.load_file("#{root_loc}/dev-env-project/configuration.yml")
 
-  if not File.exists?("#{root_loc}/.dependencies.yml")
-    require 'yaml'
-    root_loc = root_loc
-    # Load configuration.yml into a Hash
-    config = YAML.load_file("#{root_loc}/dev-env-project/configuration.yml")
-
-    # Put all the dependencies for all apps into an array
-    dependency_list = []
-    config["applications"].each do |appname, appconfig|
-      # Load any dependencies into the docker compose list
-      if appconfig.key?("dependencies")
-        appconfig["dependencies"].each do |appdependency|
-          dependency_list.push(appdependency)
-        end
+  # Put all the dependencies for all apps into an array
+  dependency_list = []
+  config["applications"].each do |appname, appconfig|
+    # Load any dependencies into the docker compose list
+    if appconfig.key?("dependencies")
+      appconfig["dependencies"].each do |appdependency|
+        dependency_list.push(appdependency)
       end
     end
-
-    # Remove duplicate dependencies
-    dependency_list = dependency_list.uniq
-
-    # Now loop through the unique list of depeencies and create a hash which will
-    # become the .depenecies.yml file
+  end
+  
+  # Remove duplicate dependencies
+  dependency_list = dependency_list.uniq
+    
+  if File.exists?("#{root_loc}/.dependencies.yml")
+    dependency_file = YAML.load_file("#{root_loc}/.dependencies.yml")
+    # Check if each dependency we want is already there or not, add it if not
+    dependency_list.each do |appdependency|
+      if is_dependency?(root_loc, appdependency) == false
+        dependency = {"#{appdependency}" => {"provisioned" => false}}
+        dependency_file["dependencies"].push(dependency)
+      end
+    end
+  else
+    # Loop through the unique list of dependencies and create a hash which will
+    # become the .dependencies.yml file
     dependency_file = {"dependencies" => []}
     dependency_list.each do |appdependency|
       dependency = {"#{appdependency}" => {"provisioned" => false}}
       dependency_file["dependencies"].push(dependency)
     end
-
-    # Write the dependency information to a file
-    File.open("#{root_loc}/.dependencies.yml", 'w') {|f| f.write(dependency_file.to_yaml)}
   end
+  
+  # Write the dependency information to a file
+  File.open("#{root_loc}/.dependencies.yml", 'w') {|f| f.write(dependency_file.to_yaml)}
 end
 
 def dependency_provisioned?(root_loc, dependency)
@@ -44,7 +52,7 @@ def dependency_provisioned?(root_loc, dependency)
 
     dependencies["dependencies"].each do |dependency_info|
       dependency_info.each do |name, info|
-        if name = dependency
+        if name == dependency
           dependency_provisioned = info["provisioned"]
           break
         end
@@ -63,7 +71,7 @@ def set_dependency_provision_status(root_loc, dependency, status)
 
     dependencies["dependencies"].each do |dependency_info|
         dependency_info.each do |name, info|
-          if name = dependency
+          if name == dependency
             info["provisioned"] = status
 
             # Write the dependency information to a file and then break from loop
@@ -85,7 +93,7 @@ def is_dependency?(root_loc, dependency)
 
     dependencies["dependencies"].each do |dependency_info|
       dependency_info.each do |name, info|
-        if name = dependency
+        if name == dependency
           is_dependency = true
           break
         end
