c62eea6b60de0a8548bdd1c98e139220e376e20d
SimonChapman <simon.chapman@landregistry.gov.uk>
2016-08-19 11:25:28 +0100

    Increase accuracy of messages during commodities file management

--- scripts/host/commodities.rb
+++ scripts/host/commodities.rb
@@ -31,30 +31,30 @@ if !File.exist?("#{root_loc}/apps/#{appname}/configuration.yml")
   # Remove duplicate commodities
   commodity_list = commodity_list.uniq
 
+  global_provisioned = []  
   if File.exists?("#{root_loc}/.commodities.yml")
     commodity_file = YAML.load_file("#{root_loc}/.commodities.yml")
+    if commodity_file["version"] == '2'
+      puts colorize_green("Found a version 2 .commodities file.")
+    else
+      puts colorize_yellow("Found a version 1 .commodities file. Upgrading to version 2...")
+      # Find any commodities that are already provisioned and add them to a list for later use when building the new file
+      new_commodity_file = {"version" => "2", "commodities" => [], "applications" => { }}
+      commodity_file["commodities"].each do |old_commodity|
+        old_commodity.each do |old_commodity_name, old_commodity_info|
+          if old_commodity_info["provisioned"] == true
+            global_provisioned.push(old_commodity_name)
+          end
+        end
+      end
+      commodity_file = new_commodity_file
+    end
   else
     # Create the base file structure
+    puts colorize_green("Did not find any .commodities file. I'll create a new one (v2).'")
     commodity_file = {"version" => "2", "commodities" => [], "applications" => { }}
   end
   
-  global_provisioned = []  
-  if commodity_file["version"] == '2'
-    puts colorize_green("Found a version 2 .commodities file.")
-  else
-    puts colorize_yellow("Found a version 1 .commodities file. Upgrading to version 2...")
-    # Find any commodities that are already provisioned and add them to a list for later use when building the new file
-    new_commodity_file = {"version" => "2", "commodities" => [], "applications" => { }}
-    commodity_file["commodities"].each do |old_commodity|
-      old_commodity.each do |old_commodity_name, old_commodity_info|
-        if old_commodity_info["provisioned"] == true
-          global_provisioned.push(old_commodity_name)
-        end
-      end
-    end
-    commodity_file = new_commodity_file
-  end
-
   # Rebuild the the master list
   commodity_file["commodities"] = commodity_list
   
